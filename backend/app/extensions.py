cd0a40754c9c9ffe7b2f4c482762a8209aed482c
mo <muhammada8@outlook.com>
2025-03-15 11:36:51 -0600

    Update extensions.py to use env firebase red

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,9 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+import os
+import json
 import firebase_admin
 from firebase_admin import auth, credentials
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-cred = credentials.Certificate("firebase_admin.json")
+firebase_creds_json = os.environ.get("FIREBASE_ADMIN_CREDENTIALS")
+if not firebase_creds_json:
+    raise ValueError("FIREBASE_ADMIN_CREDENTIALS environment variable not set.")
+
+firebase_creds_dict = json.loads(firebase_creds_json)
+
+cred = credentials.Certificate(firebase_creds_dict)
 firebase_admin.initialize_app(cred)
 
 db = SQLAlchemy()
